32d8f451386f8b653ece83f016084576eb9d7c50
yeab-samuel <zaynsolomonmar@gmail.com>
2026-04-17 11:39:53 +0300

    fix: align test class name with file name

--- src/test/java/com/ctbe/yeabsirasamuel/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/ctbe/yeabsirasamuel/productservice/ProductServiceApplicationTests.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class ProductServiceTest {
+class ProductServiceApplicationTests  {
 
 	@Mock
 	private ProductRepository productRepository;
